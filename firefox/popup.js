3569c185c89efb2a1ee155a3675142f27a97e72e
ArthurHeitmann <art.tec.15@gmail.com>
2021-09-05 23:59:36 +0200

    reworked css building + added basic firefox extension options #1

---
+++ firefox/popup.js
@@ -0,0 +1,4 @@
+// firefox/popup.ts
+var input = document.querySelector("input");
+input.oninput = () => browser.storage.local.set({ enabled: input.checked });
+browser.storage.local.get("enabled").then(({ enabled }) => input.checked = enabled);
